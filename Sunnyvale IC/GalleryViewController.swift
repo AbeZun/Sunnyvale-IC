ced19041b857c0ddec3f1a9629244267a05d61dd
Abraham Zuniga <bandg3ek@gmail.com>
2019-11-08 09:24:26 -0800

    Team Page / Gallery Page / Connect card are all working.

---
+++ Sunnyvale IC/GalleryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GalleryViewController.swift
+//  Sunnyvale IC
+//
+//  Created by Abraham Zuniga on 11/8/19.
+//  Copyright © 2019 Abraham Zuniga. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class GalleryViewController: UIViewController {
+    @IBOutlet weak var WebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "http://sunnyvaleic.com/picturesapp")
+        let request = URLRequest(url: url!)
+        
+        WebView.load(request)
+        
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
