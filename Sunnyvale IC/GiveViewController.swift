41f967d3543d5b7591682277db66918a99248687
Abraham Zuniga <bandg3ek@gmail.com>
2019-11-18 16:55:00 -0800

    Video(sermon's) Page and Give page are fully functional

---
+++ Sunnyvale IC/GiveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GiveViewController.swift
+//  Sunnyvale IC
+//
+//  Created by Abraham Zuniga on 11/18/19.
+//  Copyright © 2019 Abraham Zuniga. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class GiveViewController: UIViewController {
+    @IBOutlet weak var WebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = URL(string: "http://sunnyvaleic.com/new-give-page-app/")
+        let request = URLRequest(url: url!)
+        
+        WebView.load(request)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
